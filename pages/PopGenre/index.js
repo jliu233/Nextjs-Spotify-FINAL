854c5793eea0692a6c3c2a03bebcee045318f3d7
Jiaqi Liu <estelle.liu233@gmail.com>
2023-03-31 13:58:03 -0400

    fixed ../

--- pages/PopGenre/index.js
+++ pages/PopGenre/index.js
@@ -1,6 +1,6 @@
-import Container from '../components/Container';
+import Container from '../../components/Container';
 import Heading from '../../components/Heading';
-import Layout from '../components/Layout';
+import Layout from '../../components/Layout';
 
 
 const PopGenrePage = () => {
